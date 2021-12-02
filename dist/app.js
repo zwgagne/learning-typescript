e444e7b943bae7e44212a438fb25e305fc8048c1
Zacharie-William Gagné <zwgagne@outlook.com>
2021-12-02 13:21:58 -0500

    basic syntax knowledge aquired

--- dist/app.js
+++ dist/app.js
@@ -1,1 +1,12 @@
-console.log('Hello World');
+"use strict";
+var compteur = document.querySelector('#compteur');
+var i = 0;
+var increment = function (e) {
+    e.preventDefault();
+    i++;
+    var span = compteur === null || compteur === void 0 ? void 0 : compteur.querySelector('span');
+    if (span) {
+        span.innerText = i.toString();
+    }
+};
+compteur === null || compteur === void 0 ? void 0 : compteur.addEventListener('click', increment);
--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,14 @@
-console.log('Hello World');
+const compteur = document.querySelector('#compteur');
+let i = 0;
+
+const increment = (e: Event) => {
+    e.preventDefault();
+    i++;
+    const span = compteur?.querySelector('span')
+    if (span) {
+      span.innerText = i.toString();
+    }
+
+}
+
+compteur?.addEventListener('click', increment);
